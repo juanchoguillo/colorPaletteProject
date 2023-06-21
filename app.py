370b63a1e086556e6d263e66a3614f20ab3e6386
juanchoguillo <juancardona0607@gmail.com>
2023-06-21 02:42:16 -0400

    second push

--- app.py
+++ app.py
@@ -1,17 +1,31 @@
 from flask import Flask, render_template, request
+import openai
+from dotenv import dotenv_values
+
+config = dotenv_values('.env')
+openai.api_key = config["OPENAI.API_KEY"]
+
+
 #create templates folder 
 app = Flask(__name__, 
             template_folder='templates'
             )
 
 
-@app.route("/dog")
-def dog():
-    return "Wof Wof Wof!!!"
+# @app.route("/palette", methods=["POST"])
+# def prompt_to_palette():
+#     # OPEN AI COMPLETION CALL
+
+#     # RETURN LIST OF COLORS 
 
 @app.route("/")
 def index():
-    return "Hello There from flask!!!"
+    response = openai.Completion.create(
+        model="text-davinci-003",
+        prompt="Give me a funny word",
+    )
+    return response["choices"][0]["text"]
+    # return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
